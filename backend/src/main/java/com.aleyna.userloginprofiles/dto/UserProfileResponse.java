b680e33692693ae842911b72720e38a5bda06e91
Aleyna Çinkaya <aleynacinkaya1@gmail.com>
2026-01-07 16:35:29 +0300

    Add files via upload

---
+++ backend/src/main/java/com.aleyna.userloginprofiles/dto/UserProfileResponse.java
@@ -0,0 +1,50 @@
+package com.aleyna.userloginprofiles.dto;
+
+public class UserProfileResponse {
+
+    private String tcKimlikNo;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+
+    public String getTcKimlikNo() {
+        return tcKimlikNo;
+    }
+
+    public void setTcKimlikNo(String tcKimlikNo) {
+        this.tcKimlikNo = tcKimlikNo;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
